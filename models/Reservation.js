8a67d40cd3f42ec3eef59eed44f856b9326d21e1
GharbiHaithem <gharbi.haythem1988@gmail.com>
2025-11-03 23:18:47 +0100

    first commit

--- models/Reservation.js
+++ models/Reservation.js
@@ -5,7 +5,7 @@ const reservationSchema = new mongoose.Schema({
   fullname: { type: String, required: true }, // nom du client
   date: { type: String, required: true }, // ex: '2025-10-31'
   time: { type: String, required: true }, // ex: '10:00'
-  message: { type: String, required: true }, // ex: '10:00'
+  message: { type: String }, // ex: '10:00'
   services :{ type: String, required: true },
   mobile:{ type: String, required: true }
 },
